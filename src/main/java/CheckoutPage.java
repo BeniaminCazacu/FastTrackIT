9cc6b0598e47f7eacf1c3ec019e63261302b4aee
BeniaminCazacu <beny.cazacu@gmail.com>
2024-09-02 19:00:30 +0300

    Initial commit

---
+++ src/main/java/CheckoutPage.java
@@ -0,0 +1,287 @@
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static java.lang.Double.parseDouble;
+
+
+// Declaring the CheckoutPage class, which extends the BasePage class.
+// By extending BasePage, CheckoutPage inherits the WebDriver instance and the PageFactory initialization.
+public class CheckoutPage extends BasePage {
+
+    // Declaring a public WebDriverWait variable named 'wait'.
+    // WebDriverWait is used for implementing explicit waits during interactions with web elements.
+    public WebDriverWait wait;
+
+    // Constructor for the CheckoutPage class that takes a WebDriver object as an argument.
+    // This constructor calls the parent class (BasePage) constructor to initialize the WebDriver instance
+    // and sets up the PageFactory to initialize the web elements on this page.
+    public CheckoutPage(WebDriver driver) {
+        // Calling the parent class (BasePage) constructor using 'super' to initialize the WebDriver.
+        super(driver);
+
+        // Initializing the WebDriverWait object with a 10-second timeout.
+        // This will be used to wait for certain conditions or elements during test execution.
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    // Locating the search bar element using the @FindBy annotation.
+    // @FindBy is a Selenium annotation that helps locate elements on the web page.
+    // Here, the element is being located by its 'id' attribute with the value "input-search".
+    // Declare the WebElement as private to enforce encapsulation
+    // This ensures that 'searchBar' cannot be accessed directly from outside this class
+    @FindBy(id = "input-search")
+    private WebElement searchBar;
+
+    // A public method to set a value in the search bar.
+    // This method interacts with the searchBar element and sends the text "mouse" to it.
+    // Public method to interact with the private 'searchBar' element
+    // Provides controlled access to the encapsulated WebElement
+    public void setSearchBar() {
+        // Typing the word "mouse" into the search bar.
+        searchBar.sendKeys("mouse");
+    }
+
+    // Locating the search button element using the @FindBy annotation.
+    // The element is being located by its CSS selector, which identifies elements based on their classes.
+    // Here, the button has the classes "btn", "btn-light", and "btn-sm".
+
+    @FindBy(css = ".btn.btn-light.btn-sm")
+    private WebElement searchButton;
+
+    public void clickSearchButton() {
+        searchButton.click();
+    }
+
+    @FindBy(linkText = "Refined Frozen Mouse")
+    private WebElement frozenMouse;
+
+    public WebElement getFrozenMouse() {
+        return frozenMouse;
+    }
+
+    @FindBy(linkText = "Awesome Granite Chips")
+    private WebElement awesomeChipsProduct;
+
+    public void clickAwesomeChipsLink() {
+        awesomeChipsProduct.click();
+    }
+
+    @FindBy(css = ".svg-inline--fa.fa-cart-plus.fa-w-18.fa-3x")
+    private WebElement cartIcon;
+
+    public void clickCartIcon() {
+        cartIcon.click();
+    }
+
+    @FindBy(css = ".svg-inline--fa.fa-shopping-cart.fa-w-18")
+    private WebElement shoppingCartIcon;
+
+    public void clickShoppingCartIcon() {
+        shoppingCartIcon.click();
+    }
+
+    @FindBy(css = ".svg-inline--fa.fa-angle-right.fa-w-8")
+    private WebElement checkoutButton;
+
+    public void clickCheckoutButton() {
+        checkoutButton.click();
+    }
+
+    @FindBy(id = "first-name")
+    private WebElement firstName;
+
+    public void insertFirstName() {
+        firstName.sendKeys("Beniamin");
+    }
+
+    @FindBy(id = "last-name")
+    private WebElement lastName;
+
+    public void insertLastName() {
+        lastName.sendKeys("Cazacu");
+    }
+
+    @FindBy(id = "address")
+    private WebElement address;
+
+    public void insertAddress() {
+        address.sendKeys("Suceava");
+    }
+
+    @FindBy(css = ".btn.btn-success")
+    private WebElement continueCheckout;
+
+    public void clickContinueCheckout() {
+        continueCheckout.click();
+    }
+
+    @FindBy(css = ".btn.btn-success")
+    private WebElement completeOrder;
+
+    public void clickCompleteOrder() {
+        completeOrder.click();
+    }
+
+    @FindBy(css = ".text-muted")
+    private WebElement successMessage;
+
+    public WebElement getSuccessMessage() {
+        return successMessage;
+    }
+
+    @FindBy(css = ".svg-inline--fa.fa-heart.fa-w-16.fa-3x ")
+    private WebElement heartIcon;
+
+    public void clickHeartIcon() {
+        heartIcon.click();
+    }
+
+    @FindBy(css = ".fa-layers-counter.shopping_cart_badge")
+    private WebElement shoppingCartBadge;
+
+    public WebElement getShoppingCartBadge() {
+        return shoppingCartBadge;
+    }
+
+    public void clickShoppingCartBadge() {
+        shoppingCartBadge.click();
+    }
+
+    public WebElement getAwesomeChipsProduct(){
+        return awesomeChipsProduct;
+    }
+
+    @FindBy(css = ".svg-inline--fa.fa-heart-broken.fa-w-16.fa-2x ")
+    private WebElement brokenHeartIcon;
+
+    public void clickBrokenHeartIcon(){
+        brokenHeartIcon.click();
+    }
+
+    @FindBy(linkText = "Awesome Soft Shirt")
+    private WebElement awesomeShirt;
+
+    public void clickAwesomeShirt() {
+        awesomeShirt.click();
+    }
+
+    @FindBy(css = ".svg-inline--fa.fa-cart-plus.fa-w-18.fa-3x ")
+    private WebElement add2Cart;
+
+    public void clickAdd2Cart() {
+        add2Cart.click();
+    }
+
+    @FindBy(css = ".svg-inline--fa.fa-shopping-cart.fa-w-18 ")
+    private WebElement cartBtn;
+
+    public void clickCartBtn() {
+        cartBtn.click();
+    }
+
+    @FindBy(css = ".svg-inline--fa.fa-plus-circle.fa-w-16 ")
+    private WebElement plusOne;
+
+    public void clickPlusOne() {
+        plusOne.click();
+    }
+
+    @FindBy(css = ".amount-total")
+    private WebElement validationTotal;
+
+    public WebElement getValidationTotal() {
+        return validationTotal;
+    }
+
+    @FindBy(xpath = "(//td[@class='amount'])[1]")
+    private WebElement itemPrice;
+
+    public double productPrice() {
+        String amountValue = itemPrice.getText();
+        String cleanAmountValue = amountValue.replace("$", "");
+        return Double.parseDouble(cleanAmountValue);
+    }
+
+    //Compare two smartphones on emag website
+
+    @FindBy(id = "searchboxTrigger")
+    private WebElement searchBox;
+
+    public void insertTextInSearchBox(String search) {
+        searchBox.sendKeys(search);
+    }
+
+    @FindBy(css = ".btn.btn-default.searchbox-submit-button")
+    private WebElement mainSearchButton;
+
+    public void clickMainSearchButton() {
+        mainSearchButton.click();
+    }
+
+    @FindBy(xpath = "//*[@data-name = 'Telefon mobil Google Pixel 9 Pro, 256GB, 16GB RAM, 5G, Rose Quartz']")
+    private WebElement googlePixel9Pro;
+
+    public void clickFirstSmartphone() {
+        googlePixel9Pro.click();
+    }
+
+    @FindBy(xpath = "//div[@class = 'checkbox checkbox-custom']")
+    private WebElement compareCheckbox;
+
+    public void clickCompareCheckbox() {
+        compareCheckbox.click();
+    }
+
+    @FindBy(xpath = "//*[@data-name = 'Telefon mobil Samsung Galaxy S24 Ultra, Dual SIM, 12GB RAM, 512GB, 5G, Titanium Black']")
+    private WebElement samsungGalaxyS24Ultra;
+
+    public void clickSecondSmartphone() {
+        samsungGalaxyS24Ultra.click();
+    }
+
+    @FindBy(css = ".btn.btn-sm.btn-info")
+    private WebElement compareButton;
+
+    public void clickCompareButton() {
+        compareButton.click();
+    }
+
+    //Add/Delete product to/from favorite on emag
+
+    @FindBy(xpath = "//*[contains(@class, 'add-to-favorites btn') and contains(@data-product, 'Telefon mobil Google Pixel 9 Pro, 128GB, 16GB RAM, 5G, Hazel')]")
+    private WebElement addToFavoriteButton;
+
+    public void clickAddToFavoriteButton() {
+        addToFavoriteButton.click();
+    }
+
+    @FindBy(id = "my_wishlist")
+    private WebElement wishlist;
+
+    public void clickWishlist() {
+        wishlist.click();
+    }
+
+    @FindBy(css = ".products-number.hidden-xs.js-products-count")
+    private WebElement productsNumber;
+
+    public String favoriteProducts() {
+        if(productsNumber.getText().replace("1 lista", "").contains("1"))
+        {
+            return "1";
+        }
+        return "0";
+    }
+
+    @FindBy(css = ".gtm_9p2y1a.d-none.d-md-inline-block")
+    private WebElement deleteProductButton;
+
+    public void clickDeleteProductButton() {
+        deleteProductButton.click();
+    }
+}
---
+++ src/test/java/CheckoutTest.java
@@ -0,0 +1,128 @@
+import com.aventstack.extentreports.Status;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
+
+// This annotation is used to specify that the ExtentTestNGITestListener class should be used to listen to the test events.
+// The listener will generate reports based on the test results using the ExtentReports library.
+@Listeners(ExtentTestNGITestListener.class)
+
+// Declaring the CheckoutTest class, which extends the Hooks class.
+// By extending Hooks, CheckoutTest inherits the setup and teardown methods for WebDriver.
+public class CheckoutTest extends Hooks {
+
+    // Declaring a public variable of type CheckoutPage named 'checkoutPage'.
+    // This will be used to interact with the CheckoutPage object during the tests.
+    public CheckoutPage checkoutPage;
+
+    // Declaring a public variable of type WebDriverWait named 'wait'.
+    // WebDriverWait is used to explicitly wait for certain conditions or elements during test execution.
+    public WebDriverWait wait;
+
+    // Method annotated with @BeforeMethod, indicating that it will run before each test method.
+    // This method is used to set up the page objects and other necessary components before each test.
+    @BeforeMethod
+    public void SetupPageObject() {
+
+        // Initializing the checkoutPage object with the current WebDriver instance.
+        // This allows the test methods to interact with elements on the checkout page.
+        checkoutPage = new CheckoutPage(driver);
+
+        // Initializing the WebDriverWait object with the current WebDriver instance and a timeout of 30 seconds.
+        // This wait will be used to pause the execution until certain conditions are met or elements are found.
+        wait = new WebDriverWait(driver, 30);
+    }
+
+
+    @Test(description = "Tests the search functionality by searching for the keyword 'mouse'")
+    public void searchTest() {
+        checkoutPage.setSearchBar();
+        checkoutPage.clickSearchButton();
+        ExtentTestNGITestListener.getTest().log(Status.INFO, "The search engine is looking up for the keyword 'mouse'");
+    }
+
+    @Test(description = "Purchasing a simple product from a guest user")
+    public void checkoutTest() {
+        checkoutPage.clickAwesomeChipsLink();
+        checkoutPage.clickCartIcon();
+        checkoutPage.clickShoppingCartIcon();
+        checkoutPage.clickCheckoutButton();
+        checkoutPage.insertFirstName();
+        checkoutPage.insertLastName();
+        checkoutPage.insertAddress();
+        checkoutPage.clickContinueCheckout();
+        checkoutPage.clickCompleteOrder();
+        assertEquals(checkoutPage.getSuccessMessage().getText(), "Order complete");
+    }
+
+    @Test(description = "Add element to wishlist")
+    public void wishlistTest(){
+        checkoutPage.clickAwesomeChipsLink();
+        checkoutPage.clickHeartIcon();
+        assertEquals(checkoutPage.getShoppingCartBadge().getText(), "1");
+        ExtentTestNGITestListener.getTest().log(Status.PASS, "Shopping cart badge was updated with success.");
+        checkoutPage.clickShoppingCartBadge();
+        assertEquals(checkoutPage.getAwesomeChipsProduct().getText(), "Awesome Granite Chips");
+        checkoutPage.clickBrokenHeartIcon();
+        ExtentTestNGITestListener.getTest().log(Status.PASS, "Awesome granite chips product was found in the wishlist.");
+        //assertNotEquals(checkoutPage.getAwesomeChipsProduct().getText(), "Awesome Granite Chips");
+    }
+
+    @Test(description = "Increase the amount of a product")
+    public void increasedAmountTest() {
+        checkoutPage.clickAwesomeShirt();
+        checkoutPage.clickAdd2Cart();
+        checkoutPage.clickCartBtn();
+        ExtentTestNGITestListener.getTest().log(Status.INFO, "The price of the product is: " + checkoutPage.productPrice());
+        double expectedTotal = checkoutPage.productPrice() * 2;
+        ExtentTestNGITestListener.getTest().log(Status.INFO, "The price of the product after quantity increase should be: " + expectedTotal);
+        checkoutPage.clickPlusOne();
+        assertEquals(checkoutPage.productPrice(), expectedTotal);
+        ExtentTestNGITestListener.getTest().log(Status.INFO, "The price of the product matches expected total " + checkoutPage.productPrice() + " = " + expectedTotal);
+    }
+
+    @Test(description = "Compare two smartphones on www.emag.ro")
+    public void compareTwoSmartphones() throws InterruptedException {
+        checkoutPage.insertTextInSearchBox("google pixel 9 pro");
+        Thread.sleep(1000);
+        checkoutPage.clickMainSearchButton();
+        Thread.sleep(1000);
+        checkoutPage.clickFirstSmartphone();
+        Thread.sleep(1000);
+        checkoutPage.clickCompareCheckbox();
+        Thread.sleep(1000);
+        ExtentTestNGITestListener.getTest().log(Status.INFO, "First smartphone is added to comparison");
+        checkoutPage.insertTextInSearchBox("samsung galaxy s24 ultra");
+        Thread.sleep(1000);
+        checkoutPage.clickMainSearchButton();
+        Thread.sleep(10000);//page takes longer to load
+        checkoutPage.clickSecondSmartphone();
+        Thread.sleep(1000);
+        checkoutPage.clickCompareCheckbox();
+        Thread.sleep(1000);
+        ExtentTestNGITestListener.getTest().log(Status.INFO, "Second smartphone is added to comparison");
+        checkoutPage.clickCompareButton();
+        Thread.sleep(1000);
+    }
+
+    @Test(description = "Add//Delete product to/from favorite on emag")
+    public void AddToFavorite() throws InterruptedException {
+        checkoutPage.insertTextInSearchBox("google pixel 9 pro");
+        Thread.sleep(1000);
+        checkoutPage.clickMainSearchButton();
+        Thread.sleep(10000);
+        checkoutPage.clickAddToFavoriteButton();
+        Thread.sleep(1000);
+        checkoutPage.clickWishlist();
+        Thread.sleep(1000);
+        assertEquals(checkoutPage.favoriteProducts(), "1");
+        checkoutPage.clickDeleteProductButton();
+        Thread.sleep(1000);
+        assertEquals(checkoutPage.favoriteProducts(), "0");
+    }
+}
+
